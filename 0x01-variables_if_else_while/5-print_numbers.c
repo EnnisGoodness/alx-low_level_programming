e5b9f8be01070de91866b005422119302996967f
EnnisGoodness <racyenny@gmail.com>
2022-09-09 11:32:06 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - program that prints all single digit numbers of base 10 starting from
  * All your code should be in the main function
  * Return: 0
  */
+
 int main(void)
 {
 int n;
@@ -14,4 +16,4 @@ n += 1;
 }
 printf("\n");
 return (0);
-} 
+}
