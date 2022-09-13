ad3c3c6086f14854fade6f1d6180ee87881562be
EnnisGoodness <racyenny@gmail.com>
2022-09-13 12:59:39 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - main block
  * Description: computes and prints the sum of all the multiples of 3 or
@@ -8,18 +9,16 @@
  */
 int main(void)
 {
-	int c = 0;
-	int sum = 0;
-
-	while (c < 1024)
-	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-
-		c++;
-	}
-	printf("%i\n", sum);
-	return (0);
-
+int c = 0;
+int sum = 0;
+while (c < 1024)
+{
+if (c % 3 == 0 || c % 5 == 0)
+{
+sum += c;
+}
+c++;
+}
+printf("%i\n", sum);
+return (0);
+}
