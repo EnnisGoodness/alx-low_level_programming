fa48176bb34fa62a600177438f519e18f9a7c656
EnnisGoodness <racyenny@gmail.com>
2022-10-12 18:42:08 -0700

    my correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,12 +13,15 @@ int main(int argc, char *argv[])
 {
 	int arg1, arg2, result;
 	char o;
+	
 	int (*func)(int, int);
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 
@@ -30,11 +33,13 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	o = *argv[2];
+	
 	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	
 	result = func(arg1, arg2);
 
 	printf("%d\n", result);
