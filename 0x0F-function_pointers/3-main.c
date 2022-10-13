3cc8afad72eb20832e7f2a0e011cbe10dedea67e
EnnisGoodness <racyenny@gmail.com>
2022-10-12 19:02:25 -0700

    main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,15 +13,14 @@ int main(int argc, char *argv[])
 {
 	int arg1, arg2, result;
 	char o;
-	
 	int (*func)(int, int);
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 
@@ -32,18 +31,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
 	o = *argv[2];
-	
+
 	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
+
 	result = func(arg1, arg2);
 
 	printf("%d\n", result);
 
 	return (0);
 }
-
